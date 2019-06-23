e926861535b11591f7c255bce2f43368973ce9be
sorcererxw <tujipqy@gmail.com>
2019-06-23 11:11:06 +0800

    :recycle: Refine classification of BlockValue

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'https://www.notion.so/api/v3'
 
 async function _post<T>(url: string, data?: any): Promise<T> {
     return fetch(`${BASE_URL}${url}`,
@@ -36,18 +36,52 @@ export const enum RoleType {
 export const enum BlockType {
     TEXT = 'text',
     PAGE = 'page',
+    COLUMN = 'column',
     COLUMN_LIST = 'column_list',
     HEADER = 'header',
     SUB_HEADER = 'sub_header',
+    SUB_SUB_HEADER = 'sub_sub_header',
     NUMBERED_LIST = 'numbered_list',
+    BULLETED_LIST = 'bulleted_list',
+    LIST = 'list',
+    CODE = 'code',
+    COLLECTION_VIEW_PAGE = 'collection_view_page',
+    VIDEE = 'video',
+    IMAGE = 'image',
+    LOOM = 'loom',
+    QUOTE = 'quote',
+    MAPS = 'maps',
+    FIGMA = 'figma',
+    TO_DO = 'to_do',
+    DIVIDER = 'divider',
+    BOOKMARK = 'bookmark',
+    CALLOUT = 'callout',
+    TOGGLE = 'toggle',
+}
+
+export const enum TextStyleType {
+    BOLD = 'b',
+    ITALIC = 'i',
+    CODE = 'c',
+    LINK = 'a',
+    HIGH_LIGHT = 'h',
+    DELETED = 's',
 }
 
 export type UserId = string
 export type BlockId = string
 
+type RichText = [string, [TextStyleType, string?][]?][]
+
 export interface RoleEntry<T> {
     role: RoleType,
     value: T
+}
+
+export interface Cursor {
+    id: BlockId,
+    table: string,
+    index: number
 }
 
 export interface SpaceValue {
@@ -69,7 +103,7 @@ export interface SpaceValue {
     version: number
 }
 
-export interface BlockValue {
+export interface BlockValue<T extends BlockType> {
     alive: boolean,
     id: BlockId,
     parent_id: BlockId,
@@ -78,19 +112,167 @@ export interface BlockValue {
     created_time: number,
     last_edited_by: UserId,
     last_edited_time: number,
-    type: BlockType,
-    ignore_block_count: boolean,
-    content?: string[],
-    format?: { // only for page
-        page_cover: string,
-        page_cover_position: number // [0-1] proportion
-    },
-    properties: {
-        // TODO text style type
-        title?: [string, string[][]?][]
-        [name: string]: any
+    type: T,
+    version: number,
+
+    // ignore_block_count?: boolean,
+    // format?: { // only for page
+    //     table_wrap?: boolean
+    //     list_properties?: {
+    //         visible: boolean,
+    //         property: string
+    //     }[],
+    //     table_properties?: {
+    //         width: number,
+    //         visible: boolean,
+    //         property: string
+    //     }[]
+    // },
+    // properties?: {
+    //     // TODO text style type
+    //     title?: [string, [TextStyleType, string?][]?][]
+    //     list_properties?: {
+    //         visible: boolean,
+    //         property: string
+    //     }[],
+    //     [name: string]: any
+    // }
+    // query?: CollectionQuery
+}
+
+interface BaseTextBlockValue {
+    properties?: {
+        title: [string, [TextStyleType, string?][]?][]
     }
-    version: number
+}
+
+interface BasePermissionValue {
+    permissions: {
+        role: RoleType,
+        type: 'user_permission' | 'public_permission',
+        user_id?: UserId // exist when type is user_permission
+    }[]
+}
+
+interface BaseEmbedBlockType {
+    format?: {
+        block_full_width: boolean
+        block_height: number
+        block_page_width: boolean
+        block_preserve_scale: boolean
+        block_width: number
+        display_source: string
+    }
+    properties?: {
+        caption?: string[][],
+        source?: string[][]
+    }
+}
+
+export interface BulletedListBlockValue
+    extends BlockValue<BlockType.BULLETED_LIST>, BaseTextBlockValue {
+    content?: BlockId[]
+}
+
+export interface NumberedListBlockValue
+    extends BlockValue<BlockType.NUMBERED_LIST>, BaseTextBlockValue {
+    content?: BlockId[]
+}
+
+export interface ToDoBlockValue extends BlockValue<BlockType.TO_DO> {
+    properties?: {
+        title?: RichText
+        checked?: ['Yes' | 'No'][]
+    }
+}
+
+export interface DividerBlockValue extends BlockValue<BlockType.DIVIDER> {
+}
+
+export interface CodeBlockValue extends BlockValue<BlockType.CODE> {
+    properties?: {
+        language: string[][] // for code block to specific programming language
+        title?: RichText
+    }
+}
+
+export interface ToggleBlockValue extends BlockValue<BlockType.TOGGLE>, BaseTextBlockValue {
+    content?: BlockId[]
+}
+
+export interface ImageBlockValue extends BlockValue<BlockType.IMAGE> {
+    format?: {
+        block_aspect_ratio: number // 0.7289156626506024
+        block_full_width: boolean
+        block_height: number // 363
+        block_page_width: boolean
+        block_preserve_scale: boolean
+        block_width: number // 498
+        display_source: string /* "https://s3-us-west-2.amazonaws.com/secure.notion-static.com/
+        b10b10c5-6817-41fb-a66a-6f39289b29c4/Untitled.png" */
+    }
+    properties: {
+        source: string[][]
+    }
+    file_ids?: string[]
+}
+
+export interface TextBlockValue extends BlockValue<BlockType.TEXT>, BaseTextBlockValue {
+}
+
+export interface QuoteBlockValue extends BlockValue<BlockType.QUOTE>, BaseTextBlockValue {
+}
+
+export interface HeaderBlockValue extends BlockValue<BlockType.HEADER>, BaseTextBlockValue {
+}
+
+export interface SubHeaderBlockValue extends BlockValue<BlockType.SUB_HEADER>, BaseTextBlockValue {
+}
+
+export interface SubSubHeaderBlockValue
+    extends BlockValue<BlockType.SUB_SUB_HEADER>, BaseTextBlockValue {
+}
+
+export interface PageBlockValue extends BlockValue<BlockType.PAGE>, BasePermissionValue {
+    content: string[],
+    format: {
+        page_icon?: string
+        page_cover?: string,
+        page_cover_position?: number // [0-1] proportion
+    }
+}
+
+export interface BookmarkBlockValue extends BlockValue<BlockType.BOOKMARK> {
+    format?: {
+        bookmark_cover: string
+        bookmark_icon: string
+    }
+    properties?: {
+        description?: string[][]
+        link?: string[][]
+        title?: string[][]
+    }
+}
+
+export interface CalloutBlockValue extends BlockValue<BlockType.CALLOUT>, BaseTextBlockValue {
+    format?: {
+        block_color: string // "gray_background"
+        page_icon: string // emoji or link
+    }
+}
+
+export interface CollectionViewPageBlockValue
+    extends BlockValue<BlockType.COLLECTION_VIEW_PAGE>, BasePermissionValue {
+    view_ids: string[]
+}
+
+export interface LoomBlockValue extends BlockValue<BlockType.LOOM>, BaseEmbedBlockType {
+}
+
+export interface MapsBlockValue extends BlockValue<BlockType.MAPS>, BaseEmbedBlockType {
+}
+
+export interface FigmaBlockValue extends BlockValue<BlockType.FIGMA>, BaseEmbedBlockType {
 }
 
 export interface CollectionValue {
@@ -133,19 +315,19 @@ export interface CollectionViewValue {
 
 export interface CollectionQuery {
     filter_operator: string,
-    filter: {
+    filter?: {
         comparator: string,
         id: string,
         property: string,
         type: string
     }[],
-    sort: {
+    sort?: {
         id: string,
         type: string,
         property: string,
         direction: string
     }[],
-    aggregate: {
+    aggregate?: {
         id: string,
         aggregation_type: string,
         property: string,
@@ -182,11 +364,11 @@ export async function loadPageChunk(data: {
     pageId: string,
     verticalColumns: boolean,
     // TODO: update cursor
-    cursor: { stack: [] }
+    cursor: { stack: Cursor[] }
 }): Promise<{
-    cursor: { stack: [] }
+    cursor: { stack: Cursor[] }
     recordMap: {
-        block: { [blockId: string]: RoleEntry<BlockValue> },
+        block: { [blockId: string]: RoleEntry<BlockValue<BlockType>> },
         notion_user: { [userId: string]: RoleEntry<NotionUserValue> },
         space: { [spaceId: string]: RoleEntry<SpaceValue> }
     }
@@ -207,7 +389,7 @@ export async function queryCollection(data: {
     query: CollectionQuery
 }): Promise<{
     recordMap: {
-        block: { [blockId: string]: RoleEntry<BlockValue> }
+        block: { [blockId: string]: RoleEntry<BlockValue<BlockType>> }
         collection: { [collectionId: string]: RoleEntry<CollectionValue> }
         collection_view: { [viewId: string]: RoleEntry<CollectionViewValue> }
         space: { [spaceId: string]: RoleEntry<SpaceValue> }
@@ -215,7 +397,7 @@ export async function queryCollection(data: {
     result: {
         type: string,
         total: number,
-        blockIds: string[],
+        blockIds: BlockId[],
         aggregationResults: {
             id: string,
             value: number
